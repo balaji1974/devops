09435873a037e2497142b980f3ca6ab687d0a306
Balaji <thiagarajan.balaji@gmail.com>
2021-06-14 01:09:06 +0300

    Multiple Instances and Load Balancer

--- 03 - Terraform/06-ec2-with-elb/main.tf
+++ 03 - Terraform/06-ec2-with-elb/main.tf
@@ -14,11 +14,11 @@ terraform {
 resource "aws_default_vpc" "default" {
 }
 
-
+#security group for http server
 resource "aws_security_group" "http_server_sg" {
-  name   = "http_server_sg"
+  name = "http_server_sg"
   #vpc_id = "vpc-6e76e613"
-  vpc_id= aws_default_vpc.default.id
+  vpc_id = aws_default_vpc.default.id
 
   ingress = [
     {
@@ -63,15 +63,69 @@ resource "aws_security_group" "http_server_sg" {
 
 }
 
+# security group for load balancer 
+resource "aws_security_group" "elb_sg" {
+  name = "elb_sg"
+
+  vpc_id = aws_default_vpc.default.id
+
+  ingress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = "tcp"
+      from_port        = 80
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 80
+    }
+  ]
+
+  egress = [{
+    cidr_blocks      = ["0.0.0.0/0"]
+    description      = "outbound"
+    from_port        = 0
+    ipv6_cidr_blocks = []
+    prefix_list_ids  = []
+    protocol         = -1
+    security_groups  = []
+    self             = false
+    to_port          = 0
+  }]
+
+}
+
+#create a load balancer resource
+resource "aws_elb" "elb" {
+  name            = "elb"
+  subnets         = data.aws_subnet_ids.default_subnets.ids
+  security_groups = [aws_security_group.elb_sg.id]
+  instances       = values(aws_instance.my_ec2_http_servers).*.id
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+}
+
+
 # Create an EC2 instance 
-resource "aws_instance" "my_ec2_http_server" {
+resource "aws_instance" "my_ec2_http_servers" {
   #ami             = "ami-0aeeebd8d2ab47354"
   ami             = data.aws_ami.aws-linux-2-latest.id
   key_name        = "my-default-ec2-keypair"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.http_server_sg.id]
   #subnet_id       = "subnet-c5591be4"
-  subnet_id       = tolist(data.aws_subnet_ids.default_subnets.ids)[3]
+  for_each  = data.aws_subnet_ids.default_subnets.ids
+  subnet_id = each.value
+
+  tags = {
+    name : "my_ec2_http_servers_${each.value}"
+  }
 
   # make a connection to the created EC2 instance 
   connection {
@@ -83,8 +137,8 @@ resource "aws_instance" "my_ec2_http_server" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum install httpd -y",                                                                   # install the http server 
-      "sudo service httpd start",                                                                    # start the http server
+      "sudo yum install httpd -y",                                                                                         # install the http server 
+      "sudo service httpd start",                                                                                          # start the http server
       "echo Welcome to my virtual server Havisha and Haasya at ip ${self.public_dns}  | sudo tee /var/www/html/index.html" # create a html file 
     ]
   }
---
+++ 03 - Terraform/06-ec2-with-elb/variables.tf
@@ -0,0 +1,3 @@
+variable "ec2_key_pair" {
+  default = "~/aws/aws_keys/my-default-ec2-keypair.pem"
+}
